eba0db455a2489db6edf301be952c6370da85158
kalyakinaa <nastyakalyakinann@gmail.com>
2024-03-10 23:43:58 +0300

    Update 1.3.1

---
+++ base_test/test_polynom.cpp
@@ -0,0 +1,62 @@
+#include "Polinom.h"
+#include <gtest.h>
+
+TEST(Polinom, can_create_polinom)
+{
+	ADD_FAILURE();
+}
+
+TEST(Polinom, throws_when_try_to_create_polinom_with_too_big_max_degree)
+{
+	ADD_FAILURE();
+}
+
+TEST(Polinom, throws_when_try_to_create_polinom_with_too_small_max_degree)
+{
+	ADD_FAILURE();
+}
+
+TEST(Polinom, throws_when_try_to_create_polinom_with_incorrect_variable)
+{
+	ADD_FAILURE();
+}
+
+TEST(Polinom, can_create_copy)
+{
+	ADD_FAILURE();
+}
+
+TEST(Polinom, can_invert_polinom_to_string)
+{
+	ADD_FAILURE();
+}
+
+TEST(Polinom, can_assign_one_polinom_to_another)
+{
+	ADD_FAILURE();
+}
+
+TEST(List, can_add_one_polinom_to_another)
+{
+	ADD_FAILURE();
+}
+
+TEST(List, can_multiply_one_polinom_to_another)
+{
+	ADD_FAILURE();
+}
+
+TEST(List, can_integrate_polinom)
+{
+	ADD_FAILURE();
+}
+
+TEST(List, can_differentiate_polinom)
+{
+	ADD_FAILURE();
+}
+
+TEST(List, can_calulate_polinom)
+{
+	ADD_FAILURE();
+}
